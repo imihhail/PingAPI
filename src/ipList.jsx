79ad7a8cab78d4f9bc1a4ebbd8b7c43ef4189605
Ivar Mihhailov <ivarton@hotmail.com>
2026-03-25 20:38:33 +0200

    ipcount

--- src/ipList.jsx
+++ src/ipList.jsx
@@ -10,7 +10,6 @@ function IpList({ ipLength, setIpLength, isPinging, setIsPinging }) {
     const [speed_Mbps, setSpeed_Mbps] = useState("")
     const [selectedIpLog, setSelectedIpLog] = useState({id: null, isExpanded: false});
     const logBoxRef = useRef(null);
-    const pingText  = useRef(null)
 
 
     useLayoutEffect(() => {     
@@ -70,20 +69,16 @@ function IpList({ ipLength, setIpLength, isPinging, setIsPinging }) {
         } 
     }
 
-    async function ping() {
-        console.log(pingText);
-        
+    async function ping() {        
         let isSpeedTestRunning = false
         
-        await window.storeAPI.set(`Locations.${currentLoc.location}`, ipPartsList.map(({id, ip}) => ({ id, ip })));
+        await window.storeAPI.set(`Locations.${currentLoc.location}`, ipPartsList);
 
         const ipAddresses = ipPartsList
             .map(item => (item.ip.some(o => o === "") ? null :
             {...item, ip: item.ip.join(".") })).filter(Boolean); // removes null/undefined/empty
         
         window.startPig.sendIP(ipAddresses)
-        console.log(ipAddresses);
-        
         window.startPig.clearPingListeners();
         window.startPig.onPing(pingResp => {
             // if (!isSpeedTestRunning && pingResp[0].connection) {
@@ -108,7 +103,7 @@ function IpList({ ipLength, setIpLength, isPinging, setIsPinging }) {
         setIsPinging(true)
     }
 
-    function stopPing() {
+    function stopPing() {       
         controllerRef.current?.abort();
         setIsPinging(false)
         window.startPig.stopPing()
@@ -116,9 +111,7 @@ function IpList({ ipLength, setIpLength, isPinging, setIsPinging }) {
     }
 
     function resizeLog(y) {      
-        if (ipPartsList[y].ipLog && ipPartsList[y].ipLog.length > 0) {
-            console.log("iplog: ", ipPartsList[y].ipLog );
-            
+        if (ipPartsList[y].ipLog && ipPartsList[y].ipLog.length > 0) {            
             setSelectedIpLog(prev => ({
                 id: y,
                 isExpanded: !prev.isExpanded
@@ -152,7 +145,7 @@ function IpList({ ipLength, setIpLength, isPinging, setIsPinging }) {
                         ))}
                     </div>
                     <div onClick={() => resizeLog(y)} className='pingLog'>
-                        <span className="pingStats" id={y} ref = {pingText}>
+                        <span className="pingStats" id={y}>
                             {ipPartsList[y].speed ? (
                                 <>
                                 <span className="pingStats_ping">Ping: <strong>{ipPartsList[y].speed}ms</strong></span>
